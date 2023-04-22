00b88b55f65c11a1105cc4add27bac00610bd80b
AmjadGhoufrane <iusehaxbro@gmal.com>
2023-04-22 16:13:39 +0000

    Initial commit

--- joueur.py
+++ joueur.py
@@ -1,7 +1,16 @@
 from parametres import *
 import math
+import pygame
+
 
 class Joueur:
-    def __init__(self):
-        self.angle = 0
-        self.fov = FOV
+    def __init__(self, jeu):
+        self.jeu = jeu
+        self.angle = P_ANGLE
+        self.x, self.y = P_POS
+
+    def mouvement(self):
+        pass
+
+    def update(self):
+        self.mouvement()
--- parametres.py
+++ parametres.py
@@ -1,6 +1,9 @@
 import math
 
-xd, yd = 1000, 800
+xd, yd = 1700, 900
 FPS = 60
 FOV = math.pi / 3  # FOV de 60°
-RAYONS = 512
+P_POS = 1.5,5
+P_ANGLE = 0
+P_VITESSE = 0.004
+P_VITESSE_ROTATION = 0.002
