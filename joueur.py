d6d577228ceb6107d69b358482812d273088e344
AmjadGhoufrane <iusehaxbro@gmal.com>
2023-04-24 00:37:41 +0100

    Initial commit

--- joueur.py
+++ joueur.py
@@ -10,7 +10,56 @@ class Joueur:
         self.x, self.y = P_POS
 
     def mouvement(self):
-        pass
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+        dx, dy = 0, 0
+        speed = P_VITESSE * self.jeu.delta_time
+        speed_sin = speed * sin_a
+        speed_cos = speed * cos_a
+
+        keys = pygame.key.get_pressed()
+        num_key_pressed = -1
+
+        if keys[pygame.K_z]:
+            num_key_pressed += 1
+            dx += speed_cos
+            dy += speed_sin
+        if keys[pygame.K_s]:
+            num_key_pressed += 1
+            dx += -speed_cos
+            dy += -speed_sin
+        if keys[pygame.K_q]:
+            num_key_pressed += 1
+            dx += speed_sin
+            dy += -speed_cos
+        if keys[pygame.K_d]:
+            num_key_pressed += 1
+            dx += -speed_sin
+            dy += speed_cos
+        if self.collisions(int(self.x + dx), int(self.y + dy)):
+            self.x += dx
+            self.y += dy
+
+        if keys[pygame.K_LEFT]:
+            self.angle -= P_VITESSE_ROTATION * self.jeu.delta_time
+        if keys[pygame.K_RIGHT]:
+            self.angle += P_VITESSE_ROTATION * self.jeu.delta_time
+
+    def collisions(self, x, y):
+        return (x, y) not in self.jeu.carte.carte_dict
+
+    def draw(self):
+        pygame.draw.line(self.jeu.fenetre, 'yellow', (self.x * 100, self.y * 100),
+                         (self.x * 100 + xd * math.cos(self.angle),
+                          self.y * 100 + xd * math.sin(self.angle)), 2)
+        pygame.draw.circle(self.jeu.fenetre, 'green', (self.x * 100, self.y * 100), 15)
 
     def update(self):
         self.mouvement()
+        self.draw()
+
+    def pos(self):
+        return self.x, self.y
+
+    def case(self):
+        return int(self.x), int(self.y)
--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import sys
 import math
 from parametres import *
 from carte import *
+from joueur import *
 
 pygame.init()
 
 
 class Mainlanceur:
-    def __init__(self, xd, yd):
-        self.fenetre = pygame.display.set_mode((xd, yd))
+    def __init__(self, xd_lc, yd_lc):
+        self.carte = Carte(self)
+        self.joueur = Joueur(self)
+        self.delta_time = 1
+        self.fenetre = pygame.display.set_mode((xd_lc, yd_lc))
         self.continuer = True
         self.clock = pygame.time.Clock()
         self.map = Carte(self)
@@ -21,7 +25,7 @@ class Mainlanceur:
         pygame.display.set_caption(f'{self.clock.get_fps():.1f}')   # Permet d'afficher les fps en live à la place du nom de la fenetre
         self.fenetre.fill('black')
         self.map.draw()
-
+        self.joueur.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:  # Permet de gérer un clic sur le bouton de fermeture de la fenêtre
                 self.continuer = False
