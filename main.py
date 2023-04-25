f720202303ec32eccabbfe7547618bc174923ec1
AmjadGhoufrane <iusehaxbro@gmal.com>
2023-04-25 19:39:06 +0100

    Initial commit

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import math
 from parametres import *
 from carte import *
 from joueur import *
+from raycaster import *
 
 pygame.init()
 
@@ -12,6 +13,7 @@ class Mainlanceur:
     def __init__(self, xd_lc, yd_lc):
         self.carte = Carte(self)
         self.joueur = Joueur(self)
+        self.raycasteur = raycasteur(self)
         self.delta_time = 1
         self.fenetre = pygame.display.set_mode((xd_lc, yd_lc))
         self.continuer = True
@@ -21,11 +23,12 @@ class Mainlanceur:
 
     def Main(self):
         self.clock.tick(FPS)
-        pygame.display.flip()
         pygame.display.set_caption(f'{self.clock.get_fps():.1f}')   # Permet d'afficher les fps en live à la place du nom de la fenetre
         self.fenetre.fill('black')
         self.map.draw()
+        self.raycasteur.update()
         self.joueur.update()
+        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:  # Permet de gérer un clic sur le bouton de fermeture de la fenêtre
                 self.continuer = False
